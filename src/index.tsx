7a9cdcaaa07e224f00cc262468075b4b0ef02c54
Maxim Samoilov <nitive@icloud.com>
2026-03-01 18:14:25 +0400

    Add 2 panels

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,35 @@
-import { render, Text } from "ink"
+import process from "node:process"
+import { Box, render, Text } from "ink"
 import { useExitHandlers } from "./hooks/exit.js"
 
 const App = () => {
   useExitHandlers()
 
-  return <Text color="green">Hello world</Text>
+  const width = process.stdout.columns || 80
+  const height = process.stdout.rows || 24
+
+  return (
+    <Box width={width} height={height} flexDirection="row">
+      <Box
+        width="50%"
+        height="100%"
+        borderStyle="single"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>hello</Text>
+      </Box>
+      <Box
+        width="50%"
+        height="100%"
+        borderStyle="single"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Text>world</Text>
+      </Box>
+    </Box>
+  )
 }
 
 render(<App />)
